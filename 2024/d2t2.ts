f2f1f21c4adbb046717eae4393c4ed521f4338b8
Kevin H. <34352750+radiales@users.noreply.github.com>
2024-12-03 08:42:24 +0100

    Update d2t2.ts

--- 2024/d2t2.ts
+++ 2024/d2t2.ts
@@ -1,17 +1,17 @@
-import * as fs from 'fs';
+import * as fs from 'node:fs'
 
 // Read Data
-let fileContent = fs.readFileSync('d2.txt', 'utf8');
-let lines = fileContent.split('\n');
+let fileContent = fs.readFileSync('d2.txt', 'utf8')
+let lines = fileContent.split('\n')
 
-let list: number[][] = [];
+let list: number[][] = []
 
 // Normalize data
 for (let i = 0; i < lines.length; i++) {
-    let line = lines[i].trim();
+    let line = lines[i].trim()
     if (line.length > 0) {
         let parts = line.split(/\s+/).map(Number); 
-        list.push(parts);
+        list.push(parts)
     }
 }
 
@@ -21,31 +21,31 @@ let safeReports = 0;
 list.forEach((arrayLine) => {
     if (isSafe(arrayLine)) {
         // Safe report
-        safeReports++;
+        safeReports++
     } else if (canBeMadeSafe(arrayLine)) {
         // Report can be made safe by removing one level
-        safeReports++;
+        safeReports++
     }
 });
 
 // Function to check if a report is safe
 function isSafe(array: number[]): boolean {
-    let isGrowing = array[0] < array[1];
+    let isGrowing = array[0] < array[1]
     for (let i = 0; i < array.length - 1; i++) {
         if (!safeDistance(array[i], array[i + 1])) {
             // Distance rule violated
-            return false;
+            return false
         }
         if (isGrowing && array[i] > array[i + 1]) {
             // Growing sequence broken
-            return false;
+            return false
         }
         if (!isGrowing && array[i] < array[i + 1]) {
             // Falling sequence broken
-            return false;
+            return false
         }
     }
-    return true;
+    return true
 }
 
 // Function to check if removing one level makes the report safe
@@ -55,18 +55,18 @@ function canBeMadeSafe(array: number[]): boolean {
         const modifiedArray = array.slice(0, i).concat(array.slice(i + 1));
         if (isSafe(modifiedArray)) {
             // If the modified array is safe, the report can be made safe
-            return true;
+            return true
         }
     }
-    return false;
+    return false
 }
 
 // Check if distance between two levels is safe
 function safeDistance(first: number, second: number): boolean {
-    let distance = Math.abs(second - first);
-    return distance >= 1 && distance <= 3;
+    let distance = Math.abs(second - first)
+    return distance >= 1 && distance <= 3
 }
 
-console.log('Number of safe reports:', safeReports);
+console.log('Number of safe reports:', safeReports)
 
 // Number of safe reports: 604
